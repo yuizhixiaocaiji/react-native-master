30ca51809d221fa3e2f4a26b3553e631f6ef1c7f
yuizhixiaocaiji <1390683299@qq.com>
2023-02-07 13:50:35 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import * as React from 'react';
 import NavigatorInit from '../src/Navigator/navigatorInit';
 import {ThemeProvider} from '@rneui/themed';
 import {myTheme} from './Config';
-
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => {
+
   return (
-    <>
+    <SafeAreaProvider>
       <ThemeProvider theme={myTheme}>
         <NavigatorInit />
       </ThemeProvider>
-    </>
+    </SafeAreaProvider>
   );
 };
 
