0921b1d56a35f124b5762bd14a1c912420918aa2
yuizhixiaocaiji <1390683299@qq.com>
2023-02-08 14:19:45 +0800

    修复redux-persist报错

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,6 +1,15 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import paramsReducer from '../Slice/login.slice';
-import {persistStore, persistReducer} from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const persistConfig = {
@@ -17,6 +26,12 @@ const persistedReducer = persistReducer(persistConfig, allReducers);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 let persistore = persistStore(store);
